78ef43220d46b96a0c22c74f99cdcd173eaa0f09
chewylardo <chewylardo45@gmail.com>
2024-09-29 16:34:06 -0300

    png y splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'pages/HomeScreen.dart';
 
 void main() {
+ // WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+ // FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyApp());
 }
 
